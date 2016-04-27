d3d4e736953e99ef40214a4b55efb05565a49167
Derrick Timmermans <derrick.timmermans@outlook.com>
2016-04-27 19:49:12 +0800

    Added Python source code for variables

---
+++ 2. Variables/main.py
@@ -0,0 +1,12 @@
+s = "This is a string in Python"  # This is a string in Python
+i = 25  # This is an integer in Python
+f = 22.5  # This is a float in Python
+c = 'a'  # This is a char in Python
+
+print(i + 25)  # Returns an integer of 50
+print(i + f)  # Returns a float of 47.5
+
+# Variable types are automatically set by the compiler.
+# Floats are actually decimals.
+# String are made of chars, so they are full compatible with each other.
+# Chars are single characters.
